4bd4a81ee49e435a271bf8695fea413cc2371897
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2021-07-11 08:10:26 +1000

    abundance plot multisample

---
+++ nanopath/terminal/utils/plot_multi_abundance/commands.py
@@ -0,0 +1,41 @@
+import click
+import pandas
+from pathlib import Path
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+
+@click.command()
+@click.option(
+    "--bracken_combined", "-b", type=Path, help="Multi sample Bracken abundance estimates"
+)
+@click.option(
+    "--plot_file", "-p", type=Path, default="multi_abundance.pdf", help="Plot output file [multi_abundance.pdf]"
+)
+def plot_multi_abundance(
+    bracken_combined, plot_file
+):
+
+    """ Plot a multiple sample Bracken output"""
+
+    nrow, ncol = 1, 1
+
+    fig, ax = plt.subplots(
+        nrows=nrow, ncols=ncol, figsize=(
+            nrow*7, ncol*4.5
+        )
+    )
+
+    data = pandas.read_csv(bracken_combined, sep='\t', index_col='name', header=0)
+
+    print(data)
+
+
+    sns.scatterplot(data=data, x="gdpPercap", y="lifeExp", size="pop", legend=False, sizes=(20, 2000), ax=ax)
+
+    plt.tight_layout()
+    plt.ylabel("Count\n")
+    plt.xlabel("\nAssembly")
+
+    fig.savefig(f'{plot_file}')
+
