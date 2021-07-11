f3be6d089e5ccc938dd4ace3ba4d20b8543cfe48
esteinig <eikejoachim.steinig@my.jcu.edu.au>
2021-07-12 07:33:40 +1000

    tax bubble plot

--- nanopath/terminal/utils/plot_multi_abundance/commands.py
+++ nanopath/terminal/utils/plot_multi_abundance/commands.py
@@ -137,7 +137,7 @@ def collapse_taxa(df: pandas.DataFrame, genus: bool = False, suffix: str = None)
     for group, gdata in df.groupby(df.index):
         print(f'Collapsing species in genus: {group}')
         d = gdata.apply(sum, axis=0)
-        d.index = [f"{group} spp." if genus else f"{group}"]
+        print(d)
         grouped.append(d)
 
     grouped = pandas.DataFrame(grouped)
